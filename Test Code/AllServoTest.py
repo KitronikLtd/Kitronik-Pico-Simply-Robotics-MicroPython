51b81b81b2f13372be65d75cf9c9f3a264f3ba97
JackAtKitronik <110606017+JackAtKitronik@users.noreply.github.com>
2022-12-08 09:29:01 +0000

    Update AllServoTest.py

--- Test Code/AllServoTest.py
+++ Test Code/AllServoTest.py
@@ -1,18 +1,22 @@
-#AllServoTest.py
-# test code that ramps each servo from 0-180-0 
+# AllServoTest.py
+# Test code that ramps each servo from 0-180-0
+
 from SimplyRobotics import KitronikSimplyRobotics
 import utime
 
-
 board = KitronikSimplyRobotics()
-
 
 while True:
     for degrees in range(180):
         for servo in range(8):
-            board.Servos[servo].goToPosition(degrees)
-        utime.sleep_ms(10) #ramp speed over 10x180ms => approx 2 seconds.
+            board.servos[servo].goToPosition(degrees)
+        
+        # Ramp speed over 10x180ms => approx 2 seconds.
+        utime.sleep_ms(10)
+        
     for degrees in range(180):
         for servo in range(8):
-            board.Servos[servo].goToPosition(180-degrees)
-        utime.sleep_ms(10) #ramp speed over 10x180ms => approx 2 seconds.
+            board.servos[servo].goToPosition(180 - degrees)
+        
+        # Ramp speed over 10x180ms => approx 2 seconds.
+        utime.sleep_ms(10)
