65e7c802a6f26e8c1e59d14cf13b9fd096ea8304
JackAtKitronik <110606017+JackAtKitronik@users.noreply.github.com>
2022-12-08 09:27:33 +0000

    Update AllOutputs.py

--- Test Code/AllOutputs.py
+++ Test Code/AllOutputs.py
@@ -3,54 +3,50 @@ import utime
 import _thread
 
 '''
-    uses a thread on the second core to drive the servos, whilst the first core is driving the motors
+Uses a thread on the second core to drive the servos, whilst the first core is driving the motors
 '''
 
 board = SimplyRobotics.KitronikSimplyRobotics()
-directions = ["f","r"]
+directions = ["f", "r"]
 
-def DriveServos():
+def driveServos():
     while True:
         print("servos")
+        
         for degrees in range(180):
             for servo in range(8):
-                board.Servos[servo].goToPosition(degrees)
-            utime.sleep_ms(10) #ramp speed over 10x180ms => approx 2 seconds.
+                board.servos[servo].goToPosition(degrees)
+                
+            # Ramp speed over 10x180ms => approx 2 seconds.
+            utime.sleep_ms(10)
+            
         for degrees in range(180):
             for servo in range(8):
-                board.Servos[servo].goToPosition(180-degrees)
-            utime.sleep_ms(10) #ramp speed over 10x180ms => approx 2 seconds.
+                board.servos[servo].goToPosition(180 - degrees)
+            
+            # Ramp speed over 10x180ms => approx 2 seconds.
+            utime.sleep_ms(10)
 
-_thread.start_new_thread(DriveServos,())
+_thread.start_new_thread(driveServos, ())
 
 while True:
     print ("motors")
+    
     for direction in directions:
-        for speed in range(0,100):
-            for motor in range(0,4):
-              board.Motors[motor].On(direction,speed)
-            utime.sleep_ms(100) #ramp speed over 25x100ms => approx 2.5 second.
+        for speed in range(0, 100):
+            for motor in range(0, 4):
+                board.motors[motor].on(direction, speed)
+            
+            # Ramp speed over 25x100ms => approx 2.5 second.
+            utime.sleep_ms(100)
+            
         utime.sleep_ms(100)
-        for speed in range(100,0,-1):
-            for motor in range(0,4):
-              board.Motors[motor].On(direction,speed)
-            utime.sleep_ms(100) #ramp speed over 25x100ms => approx 2.5 second.
+        
+        for speed in range(100, 0, -1):
+            for motor in range(0, 4):
+                board.motors[motor].on(direction, speed)
+            
+            # Ramp speed over 25x100ms => approx 2.5 second.
+            utime.sleep_ms(100)
+        
         utime.sleep_ms(1000)
-
- 
-
-
-'''
-while True:
-    for direction in directions:
-      for motor in range(0,4):
-           board.theMotors[motor].On(direction,100)
-      utime.sleep_ms(5000) # let motor start and run
-      for motor in range(0,4):
-        board.theMotors[motor].Off() #then crash stop
-        utime.sleep_ms(10)
-
-
-for motor in range(0,4):
-    board.theMotors[motor].On("r",90)
-    '''
