0cba7a4250ff2d68ce197de3246e804515fb173e
JackAtKitronik <110606017+JackAtKitronik@users.noreply.github.com>
2022-12-08 09:21:50 +0000

    Update AllMotorTest.py

--- Test Code/AllMotorTest.py
+++ Test Code/AllMotorTest.py
@@ -1,65 +1,68 @@
-#AllMotorTest.py
-# test code that ramps each motor 0-100-0 then changes direction and does it again.
-#all motors run at once, but with staggered timings
+# AllMotorTest.py
+# Test code that ramps each motor 0-100-0 then changes direction and does it again.
+# All motors run at once, but with staggered timings
 
 from SimplyRobotics import KitronikSimplyRobotics
 import utime
 
-
 board = KitronikSimplyRobotics()
-directions = ["f","r"]
-
+directions = ["f", "r"]
 
 while True:
     for direction in directions:
-        for speed in range(0,25):
-            board.Motors[0].On(direction, speed)
-            board.Motors[1].On(direction, 25-speed)
-            board.Motors[2].On(direction, 50-speed)
-            board.Motors[3].On(direction, 75-speed)
-            utime.sleep_ms(100) #ramp speed over 25x100ms => approx 2.5 second.
-        for speed in range(0,25):
-            board.Motors[0].On(direction, 25+speed)
-            board.Motors[1].On(direction, speed)
-            board.Motors[2].On(direction, 25-speed)
-            board.Motors[3].On(direction, 50-speed)
-            utime.sleep_ms(100) 
-        for speed in range(0,25):
-            board.Motors[0].On(direction, 50+speed)
-            board.Motors[1].On(direction, 25+speed)
-            board.Motors[2].On(direction, speed)
-            board.Motors[3].On(direction, 25-speed)
-            utime.sleep_ms(100) 
-        for speed in range(0,25):
-            board.Motors[0].On(direction, 75+speed)
-            board.Motors[1].On(direction, 50+speed)
-            board.Motors[2].On(direction, 25+speed)
-            board.Motors[3].On(direction, speed)
-            utime.sleep_ms(100) 
-        for speed in range(0,25):
-            board.Motors[0].On(direction, 100-speed)
-            board.Motors[1].On(direction, 75+speed)
-            board.Motors[2].On(direction, 50+speed)
-            board.Motors[3].On(direction, 25+speed)
-            utime.sleep_ms(100) 
-        for speed in range(0,25):
-            board.Motors[0].On( direction, 75-speed)
-            board.Motors[1].On(direction, 100-speed)
-            board.Motors[2].On(direction, 75+speed)
-            board.Motors[3].On(direction, 50+speed)
+        for speed in range(0, 25):
+            board.motors[0].on(direction, speed)
+            board.motors[1].on(direction, 25 - speed)
+            board.motors[2].on(direction, 50 - speed)
+            board.motors[3].on(direction, 75 - speed)
+            # Ramp speed over 25x100ms => approx 2.5 second.
             utime.sleep_ms(100)
+            
         for speed in range(0,25):
-            board.Motors[0].On(direction, 50-speed)
-            board.Motors[1].On(direction, 75-speed)
-            board.Motors[2].On(direction, 100-speed)
-            board.Motors[3].On(direction, 75+speed)
+            board.motors[0].on(direction, 25 + speed)
+            board.motors[1].on(direction, speed)
+            board.motors[2].on(direction, 25 - speed)
+            board.motors[3].on(direction, 50 - speed)
             utime.sleep_ms(100) 
+            
         for speed in range(0,25):
-            board.Motors[0].On(direction, 25-speed)
-            board.Motors[1].On(direction, 50-speed)
-            board.Motors[2].On(direction, 75-speed)
-            board.Motors[3].On(direction, 100-speed)
+            board.motors[0].on(direction, 50 + speed)
+            board.motors[1].on(direction, 25 + speed)
+            board.motors[2].on(direction, speed)
+            board.motors[3].on(direction, 25 - speed)
             utime.sleep_ms(100) 
-
-  
-    
+            
+        for speed in range(0,25):
+            board.motors[0].on(direction, 75 + speed)
+            board.motors[1].on(direction, 50 + speed)
+            board.motors[2].on(direction, 25 + speed)
+            board.motors[3].on(direction, speed)
+            utime.sleep_ms(100) 
+            
+        for speed in range(0,25):
+            board.motors[0].on(direction, 100 - speed)
+            board.motors[1].on(direction, 75 + speed)
+            board.motors[2].on(direction, 50 + speed)
+            board.motors[3].on(direction, 25 + speed)
+            utime.sleep_ms(100) 
+            
+        for speed in range(0,25):
+            board.motors[0].on( direction, 75 - speed)
+            board.motors[1].on(direction, 100 - speed)
+            board.motors[2].on(direction, 75 + speed)
+            board.motors[3].on(direction, 50 + speed)
+            utime.sleep_ms(100)
+            
+        for speed in range(0,25):
+            board.motors[0].on(direction, 50 - speed)
+            board.motors[1].on(direction, 75 - speed)
+            board.motors[2].on(direction, 100 - speed)
+            board.motors[3].on(direction, 75 + speed)
+            utime.sleep_ms(100) 
+            
+        for speed in range(0,25):
+            board.motors[0].on(direction, 25 - speed)
+            board.motors[1].on(direction, 50 - speed)
+            board.motors[2].on(direction, 75 - speed)
+            board.motors[3].on(direction, 100 - speed)
+            utime.sleep_ms(100) 
